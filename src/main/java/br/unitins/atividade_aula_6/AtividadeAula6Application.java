4e40338af4f3de3fe7df5df6738a34f8de44c0f8
Orley <orleymascarenhas@unitins.br>
2025-04-28 10:28:17 -0300

    Primeiro commit

---
+++ src/main/java/br/unitins/atividade_aula_6/AtividadeAula6Application.java
@@ -0,0 +1,13 @@
+package br.unitins.atividade_aula_6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeAula6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeAula6Application.class, args);
+	}
+
+}
